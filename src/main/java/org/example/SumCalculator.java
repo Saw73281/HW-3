c73a1aa1111c32dfe309ee94feb06363eb28a186
Saw535 <Saw.535.rud@gmail.com>
2024-11-25 08:17:41 +0200

    Create SumCalculator.java

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class SumCalculator {
+    public int sum(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be greater than or equal to 1");
+        }
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
